5ab0a14b2b2d1d97c930df307c625292a118d37c
Frank <frankzhou@hotmail.ca>
2020-04-01 15:44:44 -0400

    fixed an issue with heroku support on wrong port

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const configureExpress = require("./config/express");
 
 const app = configureExpress();
 
-app.listen(8000);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
-console.log("Server running on port 8000.");
+app.listen(port);
+
+console.log(`Server running on port ${port}.`);
 module.exports = app;
